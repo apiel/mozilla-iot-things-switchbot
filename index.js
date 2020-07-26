4572cf781dadcc3f81c22ffad3223a5ede82df9b
Alexandre Piel <alexandre.piel@gmail.com>
2020-07-26 15:20:10 +0200

    failed

--- index.js
+++ index.js
@@ -4,46 +4,55 @@ const {
     Thing,
     Value,
     WebThingServer,
+    Event,
 } = require('webthing');
 
 const Switchbot = require('switchbot');
 const switchbot = Switchbot('F4:CD:C6:01:F4:97');
 
-function makeThing() {
-    const thing = new Thing(
-        'urn:dev:ops:my-switchbot-1234',
-        'Switchbot',
-        ['OnOffSwitch'],
-        'Trigger Switchbot',
-    );
+class SwitchbotThing extends Thing {
+    constructor() {
+        super(
+            'urn:dev:ops:my-switchbot-1234',
+            'Switchbot',
+            ['OnOffSwitch'],
+            'Trigger Switchbot',
+        );
 
-    thing.addProperty(
-        new Property(
-            thing,
-            'on',
-            new Value(true, (on) => {
-                console.log(`change: ${on}`);
-                if (on) {
-                    switchbot.press().then(() => {
-                        console.log('press done');
-                        thing.setProperty('on', false);
-                    });
+        const onChange = async (on) => {
+            console.log(`change: ${on}`);
+            if (on) {
+                try {
+                    // throw new Error('test error');
+                    await switchbot.press();
+                    console.log('press done');
+                    this.setProperty('on', false);
+                } catch (error) {
+                    console.error(error.toString());
+                    this.addEvent(new Event(this, 'failed', error.toString()));
                 }
-            }),
-            {
+            }
+        };
+
+        this.addProperty(
+            new Property(this, 'on', new Value(true, onChange), {
                 '@type': 'OnOffProperty',
                 title: 'Switchbot',
                 type: 'boolean',
                 description: 'Press switchbot',
-            },
-        ),
-    );
-    return thing;
+            }),
+        );
+
+        this.addAvailableEvent('failed', {
+            description: 'Something went wrong with switchbot. Try again...',
+            type: 'string',
+        });
+    }
 }
 
 function runServer() {
-    const thing = makeThing();
-    const server = new WebThingServer(new SingleThing(thing), 8888);
+    const switchbotThing = new SwitchbotThing();
+    const server = new WebThingServer(new SingleThing(switchbotThing), 8888);
     process.on('SIGINT', () => {
         server
             .stop()
